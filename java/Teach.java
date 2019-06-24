4a10137984065d528455e3612f4c308f8dd9c3f8
wty1022 <510087551@qq.com>
2019-06-24 22:44:50 +0800

    第六次尝试

--- java/Teach.java
+++ java/Teach.java
@@ -1,7 +1,8 @@
 public class Teach {
     public static void main(String[] args) {
-        System.out.println("测试一下");
-        System.out.println("wty");
         System.out.println("hahaha");
+
+
+        System.out.println(455);
     }
 }
