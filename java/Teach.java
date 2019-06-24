85f02527fff09c481e54d151499fff406d6d2c4f
wty1022 <510087551@qq.com>
2019-06-24 22:53:02 +0800

    第五次尝试

--- java/Teach.java
+++ java/Teach.java
@@ -1,9 +1,5 @@
 public class Teach {
     public static void main(String[] args) {
         System.out.println("hahaha");
-
-
-        System.out.println(455);
-        System.out.println(1111);
     }
 }
