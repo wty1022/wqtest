49577366f51a0e4891e11a7d9e344d6709e4f216
wty1022 <510087551@qq.com>
2019-06-30 14:28:55 +0800

    koko

---
+++ java/Ko.java
@@ -0,0 +1,5 @@
+public class Ko {
+    public static void main(String[] args) {
+        System.out.println("sfsdsd");
+    }
+}
---
+++ java/Teach.java
@@ -0,0 +1,7 @@
+public class Teach {
+    public static void main(String[] args) {
+        System.out.println("wawawa");
+        System.out.println(2223);
+
+    }
+}
